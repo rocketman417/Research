9cfa278f18cb5afcfa4d17514bd20f0c3074ca26
rocketman417 <33293648+rocketman417@users.noreply.github.com>
2018-02-02 13:23:11 -0600

    Add files via upload

---
+++ Optimized_face_cascade_v2.py
@@ -0,0 +1,69 @@
+import os
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+
+#take a picture
+camera=PiCamera()
+rawCapture = PiRGBArray(camera)
+
+print("taking picture in \n3...")
+time.sleep(1)
+print("2...")
+time.sleep(1)
+print("1...")
+time.sleep(1)
+
+camera.capture(rawCapture, format='bgr')
+image = rawCapture.array
+print("Picture taken...")
+
+#cv2.imshow('image', image)
+cv2.waitKey(0)
+
+#face cascade
+face_cascade = cv2.CascadeClassifier ('haarcascade_frontalface_default.xml')
+
+#equalize histogram   
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#cv2.imshow('Gray', gray)
+histogram = cv2.equalizeHist(gray)
+#cv2.imshow("histogram", histogram)
+#cv2.imwrite ("histogram.jpg", histogram)
+cv2.waitKey(0)
+
+faces = face_cascade.detectMultiScale(histogram, 1.3, 5)
+for (x,y,w,h) in faces:
+    cv2.rectangle(image, (x,y), (x+w, y+h), (255, 0 , 0), 2)
+cv2.imshow('cascade_image', image)
+
+#naming logic for cascade image
+user_input = raw_input('Do you want to save the final image as "cas_image_xxx.jpg"? (y/n)')
+if user_input == "y":
+    i = 1
+    while i < 10:
+        if os.path.isfile("cas_image_00" + str(i) + ".jpg") == True:
+            i += 1
+        elif os.path.isfile("cas_image_00" + str(i) + ".jpg") == False:
+            cv2.imwrite('cas_image_00' + str(i) + '.jpg', image)
+            print('image saved as "cas_img_00' + str(i) + '.jpg"')
+            break
+
+    while 9 < i < 100:
+        if os.path.isfile("cas_image_0" + str(i) + ".jpg") == True:
+            i += 1
+        elif os.path.isfile("cas_image_0" + str(i) + ".jpg") == False:
+            cv2.imwrite('cas_image_0' + str(i) + '.jpg', image)
+            print('image saved as "cas_img_0' + str(i) + '.jpg"')
+            break
+
+    if i > 99:
+        print ('error: all numbers have been exhausted; unable to write "cas_image_xxx.jpg"')
+
+elif user_input == "n":
+    print("image was not saved")
+
+    
+cv2.waitKey(0)
+cv2.destroyAllWindows()
